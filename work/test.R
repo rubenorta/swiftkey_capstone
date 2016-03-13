997fa70955eb6bd79db2af9330026460d7a74ff9
Rubén Orta <xrx0xmx@protonmail.com>
2016-03-13 21:41:34 +0100

    Adding Profanity and Token commands

--- work/test.R
+++ work/test.R
@@ -1,24 +1,36 @@
 library("tm")
+library("magrittr")
 
 setwd("/home/ruben/Development/swiftkey_capstone/work")
 txt <- "./en_US/samples"
- 
-data <- VCorpus(DirSource(txt, encoding = "UTF-8"),  
+
+# http://www.cs.cmu.edu/~biglou/resources/bad-words.txt
+
+data_ori <- VCorpus(DirSource(txt, encoding = "UTF-8"),  
                 readerControl = list(language = "lat"))
 
-data <- tm_map(data, content_transformer(tolower))
-data <- tm_map(data, stripWhitespace)
-data_clean <- tm_map(data, removePunctuation)
-data_word <- tm_map(data_clean, removeWords, stopwords("english"))
+# remove stopwords, badwords and other words
+bad_words <- read.csv("bad-words.txt", stringsAsFactors=FALSE, header = TRUE)
+bad_words <- paste(bad_words, collapse=" ")
+stop_words <- c("http", stopwords("english"), bad_words)
+data_clean <- tm_map(data, removeWords, stop_words)
 
-dtm <- DocumentTermMatrix(data_word)
+data <- tm_map(data_ori, content_transformer(tolower))
+data <- tm_map(data, stripWhitespace)
+data <- tm_map(data, removePunctuation)
+data <- tm_map(data, removeNumbers)
+data <- tm_map(data, stripWhitespace)
+
+dtm <- DocumentTermMatrix(data_clean)
+dtm2 <- as.matrix(dtm)
+frequency2 <- colSums(dtm2)
 
 # inspect
 # inspect(data[])
 # writeLines(as.character(data[[1]]))
 # identical(data[2], data_clean[2])
 
-findFreqTerms(dtm, 5)
+findFreqTerms(dtm, 5) # Terminos que aparecen mas de X
 findAssocs(dtm, "home", 0.8)
 removeSparseTerms(dtm, 0.4)
 inspect(DocumentTermMatrix(data_clean, list(dictionary = c("year", "york"))))
