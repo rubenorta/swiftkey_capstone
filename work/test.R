7b7d26ac5225de17e010edc2f71f6722010932c7
Rubén Orta <xrx0xmx@protonmail.com>
2016-03-17 00:34:07 +0100

    Adding graphs

--- work/test.R
+++ work/test.R
@@ -1,15 +1,23 @@
 library("tm")
 library("magrittr")
+library("wordcloud")
 
 setwd("/home/ruben/Development/swiftkey_capstone/work")
 txt <- "./en_US/samples"
 
-# http://www.cs.cmu.edu/~biglou/resources/bad-words.txt
+data_ori <- VCorpus(DirSource(txt, encoding = "UTF-8"), readerControl = list(language = "lat"))
 
-data_ori <- VCorpus(DirSource(txt, encoding = "UTF-8"),  
-                readerControl = list(language = "lat"))
+doc_stats <- system("wc -wml en_US/samples/*.txt", intern=TRUE)
+stats <- strsplit(doc_stats, " ")
+
+# Extracting some basic stats
+res_nm <- c(stats[[1]][7],stats[[2]][7], stats[[3]][8])  
+res_lc <- c(stats[[1]][3],stats[[2]][3], stats[[3]][3])
+res_wc <- c(stats[[1]][5],stats[[2]][5], stats[[3]][5])
+res_cc <- c(stats[[1]][6],stats[[2]][6], stats[[3]][7])
 
 # remove stopwords, badwords and other words
+# http://www.cs.cmu.edu/~biglou/resources/bad-words.txt
 bad_words <- read.csv("bad-words.txt", stringsAsFactors=FALSE, header = TRUE)
 bad_words <- paste(bad_words, collapse=" ")
 stop_words <- c("http", stopwords("english"), bad_words)
@@ -24,33 +32,34 @@ data <- tm_map(data, PlainTextDocument)
 data_clean <- tm_map(data, stripWhitespace)
 
 dtm <- DocumentTermMatrix(data_clean)
+#tdm <- TermDocumentMatrix(data_clean)
 
-doc_stats <- system("wc -wml en_US/samples/*.txt", intern=TRUE)
-stats <- strsplit(doc_stats, " ")
+res_wcl <- rowSums(as.matrix(dtm))
 
+summary_1 <- data.frame(res_nm,res_lc, res_wc, res_wcl, res_cc)
+names(summary_1)<- c("File Name","Line Counts", "Word Counts", "Clean Word Counts", "Character Counts")
+summary_1
 
-# Extracting some basic stats
-res_nm <- c(stats[[1]][7],stats[[2]][7], stats[[3]][8])  
-res_lc <- c(stats[[1]][3],stats[[2]][3], stats[[3]][3])
-res_wc <- c(stats[[1]][5],stats[[2]][5], stats[[3]][5])
-res_cc <- c(stats[[1]][6],stats[[2]][6], stats[[3]][7])
-#res_wc <- rowSums(as.matrix(dtm))
-
-summary_1 <- data.frame(res_nm,res_lc, res_wc, res_cc)
-names(summary_1)<- c("File Name","Line Counts", "Word Counts", "Character Counts")
-summar_1
-
-
-res_line <- sapply(data_ori, length) 
-
-dtm2 <- as.matrix(dtm)w
+dtm2 <- as.matrix(dtm)
 frequency <- colSums(dtm2)
 frequency <- sort(frequency, decreasing=TRUE)
 
+#histogram 
+hist(frequency, right=FALSE, main="Words", xlab="Counts in Documents")  
+
+wordcloud(data_clean, max.words = 100, random.order = FALSE)
+wordcloud(data_clean[[1]], max.words = 100, random.order = FALSE)
+wordcloud(data_clean[[2]], max.words = 100, random.order = FALSE)
+wordcloud(data_clean[[3]], max.words = 100, random.order = FALSE)
+
+inspect(tdm[1:10,1:3])
 
 
-# inspect
-# inspect(data[])
+
+
+
+
+
 # writeLines(as.character(data[[1]]))
 # identical(data[2], data_clean[2])
 
